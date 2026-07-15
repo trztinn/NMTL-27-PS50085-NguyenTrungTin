0153e59118bb4c484ff0e0120cab8240c167f524
Nguyen Trung Tin <trztinn@gmail.com>
2026-07-15 22:34:16 +0700

    Hoan thanh lab 2

--- Lab2/Bai2_TinhDiem.c
+++ Lab2/Bai2_TinhDiem.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    float diemToan, diemLy, diemHoa;
+    int n;
+    printf("Nhap so luong hoc sinh: ");
+    scanf("%d", &n);
 
-    printf(" Diem toan: ");
-    scanf("%f", &diemToan); 
-    printf(" Diem ly: ");   
-    scanf("%f", &diemLy);   
-    printf(" Diem hoa: ");  
-    scanf("%f", &diemHoa);  
-    
-    float diemTrungBinh = ((float)(diemToan * 3 + diemLy * 2 + diemHoa) / 6);
-    printf("Diem trung binh: %.2f\n", diemTrungBinh);
-    
+    char name[n][50];
+    float diemTB[n];
+
+    for (int i = 0; i < n; i++) {
+        float toan, ly, hoa;
+
+        printf("\nNhap ten hoc sinh thu %d: ", i + 1);
+        scanf("%s", name[i]);
+
+        printf("Nhap diem toan: ");
+        scanf("%f", &toan);
+
+        printf("Nhap diem ly: ");
+        scanf("%f", &ly);
+
+        printf("Nhap diem hoa: ");
+        scanf("%f", &hoa);
+
+        diemTB[i] = (toan * 3 + ly * 2 + hoa) / 6;
+    }
+
+    printf("\n===== DANH SACH DIEM TRUNG BINH =====\n");
+    for (int i = 0; i < n; i++) {
+        printf("Hoc sinh %s co diem trung binh: %.1f\n", name[i], diemTB[i]);
+    }
+
     return 0;
-}   
+}
