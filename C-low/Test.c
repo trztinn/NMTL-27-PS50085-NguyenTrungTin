e3865f0acb186e208ec12ecb78a0d549beacded8
Nguyen Trung Tin <trztinn@gmail.com>
2026-08-05 17:08:56 +0700

    ♻️ trztinn

--- C-low/Test.c
+++ C-low/Test.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 
+void xuatSoLeMaTran(int row, int col){
+    int a[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Nhap a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    printf("So phan tu le: \n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (a[i][j] % 2 != 0)
+            {
+                printf(" a[%d][%d] = %d\n", i, j, a[i][j]);
+            }
+        }
+    }
+}
+
 int main() {
-    int a[5];
+    int row,col;
+    printf("Nhap hang: ");
+    scanf("%d", &row);
 
-    // Nhập
-    for (int i = 0; i < 5; i++) {
-        printf("Mang so %d: ",i+1);
-        scanf("%d", &a[i]);
-    }
+    printf("Nhap cot: ");
+    scanf("%d", &col);
+    
+    xuatSoLeMaTran(row, col);
 
-    printf("In so:{ ");
-    for (int i = 0; i < 5; i++) {
-        
-        printf("%d ", a[i]);
-    }
-    printf("} ");
     return 0;
 }
